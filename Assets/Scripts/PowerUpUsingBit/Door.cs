93bd0c3834bdc1a7185637d66c95784f1e71acb5
Sudiksha <sudiksha.sd1@gmail.com>
2020-03-26 16:08:18 +0530

    Added game files

---
+++ Assets/Scripts/PowerUpUsingBit/Door.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    void OnTriggerExit(Collider player)
+    {
+        GetComponent<BoxCollider>().isTrigger = false;
+    }
+}
