4cf1f4006041fbac64be446c9d9965ebc97df162
Sudiksha <sudiksha.sd1@gmail.com>
2020-04-01 02:15:32 +0530

    Implemented scoring system and added desert bitboard

--- Assets/Scripts/BitboardLand/GenerateBitboard.cs
+++ Assets/Scripts/BitboardLand/GenerateBitboard.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateBitboard : MonoBehaviour
 {
     public GameObject[] tiles;
     public GameObject tree;
     public GameObject house;
+    public Text score;
     GameObject[] tile;
     long dirtBitBoard = 0;
+    long desertBitBoard = 0;
     long treeBitBoard = 0;
     long houseBitBoard = 0;
     void Start()
@@ -23,10 +26,16 @@ public class GenerateBitboard : MonoBehaviour
                 {
                     dirtBitBoard = SetBitBoard(dirtBitBoard, i, j);
                 }
+                if (newTile.tag == "Desert")
+                {
+                    desertBitBoard = SetBitBoard(desertBitBoard, i, j);
+                }
             }
         }
-        PrintDirtbitBoard();
-        Debug.Log("Number of Dirt Tiles : " + CountDirtTile());
+        PrintbitBoard(dirtBitBoard);
+        PrintbitBoard(desertBitBoard);
+        Debug.Log("Number of Dirt Tiles : " + CountTile(dirtBitBoard));
+        Debug.Log("Number of Desert Tiles : " + CountTile(desertBitBoard));
         InvokeRepeating("PlantTree", 1, 1);   // Looks for dirt time after every 1 second and plant tree on it if found
     }
 
@@ -38,21 +47,20 @@ public class GenerateBitboard : MonoBehaviour
         {
             if (Physics.Raycast(ray, out hit))
             {
-                if (GetCellStateDirt(dirtBitBoard & (~treeBitBoard), (int)hit.collider.gameObject.transform.position.x, (int)hit.collider.gameObject.transform.position.z))
+                if (GetCellStateDirt(dirtBitBoard & (~treeBitBoard) | desertBitBoard, (int)hit.collider.gameObject.transform.position.x, (int)hit.collider.gameObject.transform.position.z))
                 {
                     GameObject houseObj = Instantiate(house, new Vector3(hit.collider.gameObject.transform.position.x, 0, hit.collider.gameObject.transform.position.z), Quaternion.identity);
                     houseBitBoard = SetBitBoard(houseBitBoard, (int)hit.collider.gameObject.transform.position.x, (int)hit.collider.gameObject.transform.position.z);
-
+                    score.text = "SCORE: " + (CountTile(houseBitBoard & dirtBitBoard) * 2 + CountTile(houseBitBoard & desertBitBoard) * 10).ToString();   //Get 2 points when house built on dirt and 10 points when built on desert
                 }
             }
         }
     }
 
-    int CountDirtTile()
+    int CountTile(long bitBoard)
     {
         int count = 0;
-
-        long dirtTile = dirtBitBoard;
+        long dirtTile = bitBoard;
         while (dirtTile != 0)
         {
             dirtTile &= dirtTile - 1;
@@ -65,7 +73,7 @@ public class GenerateBitboard : MonoBehaviour
     {
         int row = UnityEngine.Random.Range(0, 8);
         int col = UnityEngine.Random.Range(0, 8);
-        if (GetCellStateDirt((dirtBitBoard & (~houseBitBoard)), row, col))
+        if (GetCellStateDirt(((dirtBitBoard & (~houseBitBoard)) | desertBitBoard), row, col))
         {
             GameObject treeObj = Instantiate(tree, new Vector3(row, 0, col), Quaternion.identity);
             treeObj.transform.parent = tile[row * 8 + col].transform;
@@ -85,8 +93,8 @@ public class GenerateBitboard : MonoBehaviour
         return (bitBoard |= dirtBit);
     }
 
-    void PrintDirtbitBoard()
+    void PrintbitBoard(long bitBoard)
     {
-        Debug.Log(Convert.ToString(dirtBitBoard, 2).PadLeft(64, '0'));
+        Debug.Log(Convert.ToString(bitBoard, 2).PadLeft(64, '0'));
     }
 }
