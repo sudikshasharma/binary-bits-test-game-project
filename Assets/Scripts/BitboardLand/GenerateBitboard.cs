2e885df0aad832c59d77d58ea46336e83bd0194a
Sudiksha <sudiksha.sd1@gmail.com>
2020-03-31 23:46:17 +0530

    Implemented tree instantiation on dirt

--- Assets/Scripts/BitboardLand/GenerateBitboard.cs
+++ Assets/Scripts/BitboardLand/GenerateBitboard.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 public class GenerateBitboard : MonoBehaviour
 {
     public GameObject[] tiles;
+    public GameObject tree;
+    GameObject[] tile;
     long dirtBitBoard = 0;
     void Start()
     {
+        tile = new GameObject[64];
         for (int i = 0; i < 8; i++)
         {
             for (int j = 0; j < 8; j++)
             {
                 GameObject newTile = Instantiate(tiles[(UnityEngine.Random.Range(0, tiles.Length))], new Vector3(i, 0, j), Quaternion.identity);
+                tile[i * 8 + j] = newTile;
                 if (newTile.tag == "Dirt")
                 {
                     SetDirtBitBoard(i, j);
@@ -19,6 +23,25 @@ public class GenerateBitboard : MonoBehaviour
             }
         }
         PrintDirtbitBoard();
+        InvokeRepeating("PlantTree", 1, 1);   // Looks for dirt time after every 1 second and plant tree on it if found
+    }
+
+    void PlantTree()
+    {
+        int row = UnityEngine.Random.Range(0, 8);
+        int col = UnityEngine.Random.Range(0, 8);
+        if (GetCellStateDirt(row, col))
+        {
+            Debug.Log(tile[row * 8 + col].tag);
+            GameObject treeObj = Instantiate(tree, new Vector3(row, 0, col), Quaternion.identity);
+            treeObj.transform.parent = tile[row * 8 + col].transform;
+        }
+    }
+
+    bool GetCellStateDirt(int row, int col)
+    {
+        long dirtBit = 1 << (row * 8 + col);
+        return ((dirtBitBoard & dirtBit) != 0);
     }
 
     void SetDirtBitBoard(int row, int col)
